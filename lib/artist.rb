47c6cb06e8cda50ad3e1b91a93025d3d7cadeb33
taisaoko <ttd3calvin@gmail.com>
2018-09-19 18:18:35 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,5 +1,5 @@
 class Artist
-  attr_accessor :name, :songs
+  attr_accessor :name 
   @@song_count = 0
   
   def initialize(name)
@@ -7,7 +7,9 @@ class Artist
     @songs =[]
   end
   
-  
+  def songs
+    @songs
+  end
   
   def add_song(song)
     @songs << song
